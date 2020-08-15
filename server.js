479d47251530a4ff0ef44feef2d8b03162fe2c36
Rafik <rahumandev@gmail.com>
2020-08-15 10:57:46 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,46 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+// const express = require('express');
+// const path = require('path');
 
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static('./src/nexgenprints'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname,'/src/nexgenprints/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
+
+//Install express server
+const express = require('express');
 const app = express();
+const path = require('path');
+const port = process.env.PORT || 8000;
+const server = require('http').Server(app);
+
+//app.use(express.static(__dirname, 'dist', {index: false}));
+
+app.use(express.static(__dirname + '/dist/sb-admin-angular'));
+server.listen(port, function() {
+    console.log("App running on port " + port);
+})
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/sb-admin-angular', 'index.html'));
+   console.log(path.join(__dirname, 'dist/sb-admin-angular', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/sb-admin-angular', 'index.html'));
+    console.log(path.join(__dirname, 'dist/sb-admin-angular', 'index.html'));
+});
+
 
 // // Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/dist/nexgenprints'));
@@ -13,12 +51,12 @@ const app = express();
 // });
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/src/nexgenprints'));
+// app.use(express.static(__dirname + '/src'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/src/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/src/index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8000);
