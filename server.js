39e23684de66c40252dd71a95115f29dc0fdab26
Rafik <rahumandev@gmail.com>
2020-08-15 10:18:06 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const path = require('path');
 
 const app = express();
 
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/nexgenprints'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname+'/dist/nexgenprints/index.html'));
+// });
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/nexgenprints'));
+app.use(express.static(__dirname + '/src/nexgenprints'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/nexgenprints/index.html'));
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
